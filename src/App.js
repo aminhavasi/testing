6593cd554439ed37f30647b9117e091c9299e810
amin havasi <pau.ahq@gmail.com>
2020-03-22 17:35:50 +0430

    main create

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Persons from './components/persons';
+import './css/index.css';
+class App extends Component {
+    state = {
+        persons: [
+            { id: 1, name: 'amin', age: 23 },
+            { id: 2, name: 'ali', age: 23 },
+            { id: 3, name: 'hassan', age: 23 },
+            { id: 4, name: 'mohamad', age: 23 }
+        ],
+        showPersons: false
+    };
+    handleShowPersons = () => {
+        this.setState({ showPersons: !this.state.showPersons });
+        console.log(this.state.showPersons);
+    };
+    handleDeletePerson = id => {
+        const persons = [...this.state.persons];
+        const filtredPersons = persons.filter(p => p.id !== id);
+        this.setState({ persons: filtredPersons });
+    };
+    handleNameChange = (name, id) => {
+        const { persons: allPersons } = this.state;
+        const personIndex = allPersons.findIndex(p => p.id === id);
+        const person = allPersons[personIndex];
+        person.name = event.target.value;
+        const persons = [...allPersons];
+        persons[personIndex] = person;
+        this.setState({ persons });
+    };
+    render() {
+        const { persons, showPersons } = this.state;
+        let person = null;
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        if (showPersons) {
+            person = (
+                <Persons
+                    persons={persons}
+                    deletePerson={this.handleDeletePerson}
+                    personChange={this.handleNameChange}
+                />
+            );
+        }
+        return (
+            <div style={{ textAlign: 'center' }}>
+                <h2>Personal Manager</h2>
+                <h3>number person:{this.state.persons.length}</h3>
+                {person}
+                <button
+                    style={{ padding: '1em', backgroundColor: 'pink' }}
+                    onClick={this.handleShowPersons}
+                >
+                    show
+                </button>
+            </div>
+        );
+    }
 }
-
 export default App;
